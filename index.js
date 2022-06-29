0450fd2c4adb926493c898bb5bbdf07743555d09
NithyaKarthick <103762158+Nithyakarthick26@users.noreply.github.com>
2022-06-29 12:31:47 -0700

    code

---
+++ index.js
@@ -0,0 +1,20 @@
+function loadCoupon()
+{
+    document.getElementById(`coupon`).style.visibility=`visible`;
+}
+
+function closeCoupon()
+{
+    document.getElementById(`coupon`).style.visibility=`hidden`;
+}
+
+
+function loadGeo()
+{
+    document.getElementById(`out`).style.visibility=`visible`;
+}
+
+function geolocation()
+{
+    document.getElementById(`out`).style.visibility=`hidden`;
+}
